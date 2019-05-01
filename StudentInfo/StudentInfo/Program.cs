ca31b0c9221de97939931281065ba08596b0977f
seanjasm <seanjasm@gmail.com>
2019-04-30 20:38:45 -0400

    first commit

---
+++ StudentInfo/StudentInfo/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace StudentInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = GetStudents();
+            string[] studentsCityAndState = GetCityAndState();
+            Array.Sort(students);//Non practical sort. Of course in real world the address would match
+            bool repeat = true;
+
+            while (repeat)
+            {
+                Console.Clear();
+                UserInput.Display("\n\n\tWelcome to ASP.Net Student Info Center!\n\n");
+                for (int i = 0; i < students.Length; i++)
+                {
+                    UserInput.Display($"\t\t{i + 1}............  {students[i]}");
+                }
+                try
+                {
+                    int indexOfStudent = UserInput.GetUserInputAsInteger("\n\n\tEnter a " +
+                        "student number to see more:  ") - 1;
+
+                    UserInput.Display($"Name: {students[indexOfStudent]}   " +
+                        $"Location: {studentsCityAndState[indexOfStudent]}");
+
+                }
+                catch(IndexOutOfRangeException e)
+                {
+                    UserInput.Display($"This record does not exist. Please check the above list.");
+                    ErrorDetail(e);
+                }
+                catch (Exception e)
+                {
+                    UserInput.Display(e.Message);
+                    ErrorDetail(e);
+                }
+                finally
+                {                    
+                    repeat = UserInput.UserConfirmationPrompt("\n\n\tLookup another student(Y/N)? ");
+                }
+
+            }
+        }     
+        
+        public static void ErrorDetail(Exception e)
+        {
+            if (UserInput.UserConfirmationPrompt("\n\n\tDo you want to " +
+                        "see more about this error(Y/N)? "))
+            {
+                UserInput.DisplayExceptionDetail(e);
+            }
+        }
+
+        public static string[] GetStudents()
+        {
+            return new string[] { "Sean Mattis", "Jeff Ducat", "Andre Watts", "Anita Devkota", "Tony Yeho" };
+        }
+
+        public static string[] GetCityAndState()
+        {
+            return new string[] { "Detroit, MI", "Stratford, CT", "Westland, MI", "Livonia, MI", "Royal Oak, MI" };
+        }
+
+        
+    }
+}
---
+++ StudentInfo/StudentInfo/UserInput.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StudentInfo
+{
+    class UserInput
+    {
+        public static int GetUserInputAsInteger(string message)
+        {
+            Display(message);
+            string input = Console.ReadLine();
+
+            if (input == string.Empty)
+            {
+                return GetUserInputAsInteger(message);
+            }
+            try
+            {
+                return int.Parse(input);
+            }
+            catch(Exception e)
+            {
+                throw new Exception("A whole number is needed like 1, but not like 1.0!", e);
+            }
+        }
+
+
+        public static string GetUserInput(string message)
+        {
+            Display(message);
+            string input = Console.ReadLine();
+
+            if (input == string.Empty)
+            {
+                return GetUserInput(message);
+            }
+            return input;
+        }
+
+
+
+        /// <summary>
+        /// Displays a message
+        /// </summary>
+        /// <param name="message"></param>
+        public static void Display(string message)
+        {
+            Console.Write("\n\n" + message);
+        }
+
+        /// <summary>
+        /// Return true if user input equals trueOption. trueOption set to "Y" by default.  
+        /// </summary>
+        /// <param name="message"></param>  
+        public static bool UserConfirmationPrompt(string message, string trueOption = "Y", string falseOption = "N")
+        {
+
+            string input = UserInput.GetUserInput(message);
+
+            if (new Regex($"{trueOption}", RegexOptions.IgnoreCase).IsMatch(input))
+            {
+                return true;
+            }
+
+            if (new Regex($"{falseOption}", RegexOptions.IgnoreCase).IsMatch(input))
+            {
+                return false;
+            }
+            else
+            {
+                return UserConfirmationPrompt(message);
+            }
+        }
+
+        public static void DisplayExceptionDetail(Exception e)
+        {
+            Display(e.Message);
+            if(e.InnerException != null)
+            {
+                Display(e.InnerException.Message);
+                Display(e.Source);
+            }
+        }
+    }
+}
